1abeb18532b3e1ab88b61a2976420d2a2dae623e
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2022-07-25 21:16:39 -0300

    Add interpreter using environmental model

--- lambda.ml
+++ lambda.ml
@@ -1,1 +1,66 @@
-let () = print_endline "Hello, World!"
+type var = string
+type expr =
+  | Variable of { name : var }
+  | Abstraction of { param : var; body : expr; }
+  | Application of { abstraction : expr; argument : expr }
+  | Integer of int
+  | BinOp of (binop * expr * expr)
+
+and value =
+  | VClosure of { param : var; body : expr; env : env  }
+  | VInt of int
+
+and env = (var * value) list
+
+and binop =
+  | Plus
+
+let rec eval (env : env) (expr : expr) : value =
+  match expr with
+  | Variable { name } -> List.assoc name env
+  | Abstraction { param; body } -> VClosure { param; body; env }
+  | Application { abstraction; argument } -> eval_app env abstraction argument
+  | Integer v -> VInt v
+  | BinOp (bop, e1, e2) -> eval_bop env bop e1 e2
+
+and eval_app env abstraction argument =
+  let arg = eval env argument in
+  let abs = eval env abstraction in
+  match abs with
+  | VClosure { param; body; env } ->
+      let new_env = (param, arg) :: env in
+      eval new_env body
+  | _ -> failwith "Invalid function application "
+
+and eval_bop (env : env) (bop : binop) (e1 : expr) (e2 : expr) : value =
+  let v1 = get_integer (eval env e1) in
+  let v2 = get_integer (eval env e2) in
+  match bop with
+  | Plus -> VInt (v1 + v2)
+
+and get_integer = function
+  | VInt v -> v
+  | _ -> failwith "Expected integer at binary operation"
+  
+let value_to_string = function
+  | VClosure _ -> "VClosure - to_string not implemented"
+  | VInt v -> string_of_int v
+
+(* (((fun x -> fun y -> x + y) 2) 4 *)
+let program = Application {
+    abstraction = Application {
+        abstraction = Abstraction {
+            param = "x";
+            body = Abstraction {
+                param = "y";
+                body = BinOp (Plus, Variable { name = "x" }, Variable { name = "y" })
+              }
+          };
+        argument = Integer 2;
+      };
+      argument = Integer 4;
+  }
+
+let () =
+  let output = eval [] program in
+  print_endline (value_to_string output);
